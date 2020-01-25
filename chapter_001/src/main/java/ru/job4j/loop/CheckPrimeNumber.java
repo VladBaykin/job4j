d4cac582d1f6f33ddaa80b64fadb1537ebd6db75
VladBaykin <vladbaykin@gmail.com>
2020-01-25 19:11:21 +0300

    Простое число[#215052]

---
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+    public static boolean check(int finish) {
+        boolean prime = true;
+        for (int index = 2; index < finish; index++) {
+            if (finish % index == 0) {
+                prime = false;
+                break;
+            }
+        }
+        return prime;
+    }
+}
