5394b7d2cd5505db7d76e7fb4dc59254c4b7a8b3
VladBaykin <vladbaykin@gmail.com>
2020-01-26 17:39:31 +0300

    JavaDoc. Code convention[#215057]

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,19 @@
+package ru.job4j.calculate;
+
+/**
+ * Class Calculate для вычисления арифметических операций.
+ * @author Vladislav Baykin
+ * @since 0.1
+ * @version 1
+ */
+
+public class Calculate {
+
+    /**
+     * В этом методе происходит вичисление...
+     * @param args - без аргументов.
+     */
+    public static void main(String[] args) {
+
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package for calculate task.
+ * @author Vladislav Baykin (vladbaykin@gmail.com)
+ * @version 1
+ * @since 0.1
+ */
+package ru.job4j.calculate;
