6159cc778b42c9b8181cd1f8cebcb25841e8162b
VladBaykin <vladbaykin@gmail.com>
2020-02-03 12:21:05 +0300

    Дефрагментация массива [#215001]

---
+++ chapter_001/src/test/java/ru/job4j/array/DefragmentTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class DefragmentTest {
+
+    @Test
+    public void notFirstNull() {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] compressed = Defragment.compress(input);
+        String[] expected = {"I", "wanna", "be", "compressed", null, null, null};
+        Assert.assertArrayEquals(expected, compressed);
+    }
+
+    @Test
+    public void firstNull() {
+        String[] input = {null, "I", "wanna", null, "be", null, "compressed"};
+        String[] compressed = Defragment.compress(input);
+        String[] expected = {"I", "wanna", "be", "compressed", null, null, null};
+        Assert.assertArrayEquals(expected, compressed);
+    }
+}
