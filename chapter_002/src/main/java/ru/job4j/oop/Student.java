d512e5bfd3da5b64e61cff113a76ea82ec6b5f45
VladBaykin <vladbaykin@gmail.com>
2020-02-15 17:18:44 +0300

    Вызов метода объекта [#215139]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Student.java
@@ -0,0 +1,19 @@
+package ru.job4j.oop;
+
+public class Student {
+    public void music() {
+        System.out.println("I believe i can fly");
+    }
+    public void accordion() {
+        System.out.println("accordion");
+    }
+    public static void main(String[] args) {
+        Student petya = new Student();
+        petya.accordion();
+        petya.accordion();
+        petya.accordion();
+        petya.music();
+        petya.music();
+        petya.music();
+    }
+}
