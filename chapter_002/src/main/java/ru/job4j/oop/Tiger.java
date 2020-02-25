18a7b4c72e75432554b379a985691034e7548bc2
VladBaykin <vladbaykin@gmail.com>
2020-02-25 17:19:00 +0300

    Конструктор по умолчанию [#215148]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Tiger.java
@@ -0,0 +1,11 @@
+package ru.job4j.oop;
+
+public class Tiger extends Predator {
+    public Tiger() {
+        System.out.println("Tiger");
+    }
+
+    public static void main(String[] args) {
+        Tiger tiger = new Tiger();
+    }
+}
