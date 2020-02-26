c7b102db3eca3f41e4d9b225d57d80d4adfeea57
VladBaykin <vladbaykin@gmail.com>
2020-02-26 12:29:49 +0300

    Перегрузка конструктора.[#215149]

--- chapter_002/src/main/java/ru/job4j/oop/Animal.java
+++ chapter_002/src/main/java/ru/job4j/oop/Animal.java
@@ -9,5 +9,4 @@ public class Animal {
     public Animal(String name) {
         this.name = name;
     }
-
 }
--- chapter_002/src/main/java/ru/job4j/oop/Tiger.java
+++ chapter_002/src/main/java/ru/job4j/oop/Tiger.java
@@ -1,9 +1,13 @@
 package ru.job4j.oop;
 
 public class Tiger extends Predator {
+    public Tiger() {
+        super();
+        System.out.println("Tiger");
+    }
+
     public Tiger(String name) {
         super(name);
-        System.out.println("Tiger");
     }
 
     public static void main(String[] args) {
