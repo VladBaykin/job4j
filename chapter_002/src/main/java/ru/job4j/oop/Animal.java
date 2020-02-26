ad8f736b3414d4c378531aed6e33623a59d84fe4
VladBaykin <vladbaykin@gmail.com>
2020-02-26 12:14:58 +0300

    Конструктор по умолчанию [#215148] - fix

--- chapter_002/src/main/java/ru/job4j/oop/Animal.java
+++ chapter_002/src/main/java/ru/job4j/oop/Animal.java
@@ -1,13 +1,7 @@
 package ru.job4j.oop;
 
 public class Animal {
-    String name;
     public Animal() {
         System.out.println("Animal");
     }
-
-    public Animal(String name) {
-        this.name = name;
-    }
-
 }
