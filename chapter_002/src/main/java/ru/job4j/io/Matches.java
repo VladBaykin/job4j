8cfe4fbb011f18617c066c2b842efd498caaa63b
VladBaykin <vladbaykin@gmail.com>
2020-05-16 18:06:53 +0300

    Scanner и чтение числа из консоли [#215113] fix

--- chapter_002/src/main/java/ru/job4j/io/Matches.java
+++ chapter_002/src/main/java/ru/job4j/io/Matches.java
@@ -14,6 +14,7 @@ class Player {
     }
 }
 public class Matches {
+    static int match = 11;
     public static void main(String[] args) {
         Player player1 = new Player();
         Player player2 = new Player();
@@ -23,40 +24,29 @@ public class Matches {
         System.out.print("Игрок 2, введите имя: ");
         player2.setName(input.nextLine());
 
-        int match = 11;
         System.out.println("Игра начинается.");
-        System.out.println("Количество спичек: " + match);
         System.out.println();
 
-        while (true) {
-            System.out.print(player1.getName() + ", возмите от 1 до 3 спичек: ");
-            int select1 = Integer.parseInt(input.nextLine());
-            if (select1 < 1 || select1 > 3) {
+        while (match > 0) {
+            deleteMatch(player1);
+            deleteMatch(player2);
+        }
+    }
+    public static void deleteMatch(Player player) {
+        if (match > 0) {
+            System.out.println("Количество спичек: " + match);
+            System.out.println();
+            System.out.print(player.getName() + ", возмите от 1 до 3 спичек: ");
+            Scanner input = new Scanner(System.in);
+            int select = Integer.parseInt(input.nextLine());
+            if (select < 1 || select > 3) {
                 System.out.print("Ошибка! Можно брать от 1 до 3 спичек.");
             } else {
-                match = match - select1;
+                match = match - select;
             }
-            System.out.println();
-            System.out.println("Количество спичек: " + match);
             if (match <= 0) {
-                System.out.println(player1.getName() + " выиграл.");
+                System.out.println(player.getName() + " выиграл.");
                 System.out.println("Игра окончена.");
-                break;
-            }
-
-            System.out.print(player2.getName() + ", возмите от 1 до 3 спичек: ");
-            int select2 = Integer.parseInt(input.nextLine());
-            if (select2 < 1 || select2 > 3) {
-                System.out.print("Ошибка! Можно брать от 1 до 3 спичек.");
-            } else {
-                match = match - select2;
-            }
-            System.out.println();
-            System.out.println("Количество спичек: " + match);
-            if (match <= 0) {
-                System.out.println(player2.getName() + " выиграл.");
-                System.out.println("Игра окончена.");
-                break;
             }
         }
     }
