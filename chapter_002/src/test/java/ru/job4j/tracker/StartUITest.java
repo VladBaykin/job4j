4947a5c7e7633c2fe302c486f24cf31880eeaa8a
VladBaykin <vladbaykin@gmail.com>
2020-05-25 19:02:44 +0300

    Тестирование. Подготовка данных. [#215121]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -16,4 +16,26 @@ public class StartUITest {
         Item expected = new Item("Fix PC");
         assertEquals(expected.getName(), created.getName());
     }
+
+    @Test
+    public void editItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {item.getId(),"replaced item"};
+        StartUI.editItem(new StubInput(answers), tracker);
+        Item replaced = tracker.findById(item.getId());
+        assertEquals("replaced item", replaced.getName());
+    }
+
+    @Test
+    public void deleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {item.getId(), "deleted item"};
+        StartUI.deleteItem(new StubInput(answers), tracker);
+        Item deleted = tracker.findById(item.getId());
+        assertNull(deleted);
+    }
 }
