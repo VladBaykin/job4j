a8363730d4ffe0af8d9f52678a13dadf00c5bfeb
VladBaykin <vladbaykin@gmail.com>
2020-10-29 11:37:31 +0300

    Тесты вывода на консоль в StartUI [33585#215111]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -60,6 +60,124 @@ public class StartUITest {
         };
         new StartUI(out).init(in, tracker, actions);
         assertEquals("Menu." + System.lineSeparator() +
-                "0.  Exit " + System.lineSeparator(), out.toString());
+                "0.  Exit " + System.lineSeparator()
+                 + "=== Exit program ===" + System.lineSeparator(), out.toString());
+    }
+
+    @Test
+    public void whenFindAll() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"1", "6"}
+        );
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("All Items"));
+        UserAction[] actions = {
+                new CreateAction(out),
+                new FindAllAction(out),
+                new EditAction(out),
+                new DeleteAction(out),
+                new FindByIdAction(out),
+                new FindByNameAction(out),
+                new ExitAction(out),
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertEquals("Menu." + System.lineSeparator() +
+                        "0.  Create " + System.lineSeparator() +
+                        "1.  All Items " + System.lineSeparator() +
+                        "2.  Edit " + System.lineSeparator() +
+                        "3.  Delete " + System.lineSeparator() +
+                        "4.  Find by Id " + System.lineSeparator() +
+                        "5.  Find by Name " + System.lineSeparator() +
+                        "6.  Exit " + System.lineSeparator() +
+                        "All Items" + System.lineSeparator() +
+                        "Menu." + System.lineSeparator() +
+                        "0.  Create " + System.lineSeparator() +
+                        "1.  All Items " + System.lineSeparator() +
+                        "2.  Edit " + System.lineSeparator() +
+                        "3.  Delete " + System.lineSeparator() +
+                        "4.  Find by Id " + System.lineSeparator() +
+                        "5.  Find by Name " + System.lineSeparator() +
+                        "6.  Exit " + System.lineSeparator() +
+                        "=== Exit program ===" + System.lineSeparator()
+                         , out.toString());
+    }
+
+    @Test
+    public void whenFindByName() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("Found by name item"));
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"5", item.getName(), "6"}
+        );
+        UserAction[] actions = {
+                new CreateAction(out),
+                new FindAllAction(out),
+                new EditAction(out),
+                new DeleteAction(out),
+                new FindByIdAction(out),
+                new FindByNameAction(out),
+                new ExitAction(out),
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertEquals("Menu." + System.lineSeparator() +
+                        "0.  Create " + System.lineSeparator() +
+                        "1.  All Items " + System.lineSeparator() +
+                        "2.  Edit " + System.lineSeparator() +
+                        "3.  Delete " + System.lineSeparator() +
+                        "4.  Find by Id " + System.lineSeparator() +
+                        "5.  Find by Name " + System.lineSeparator() +
+                        "6.  Exit " + System.lineSeparator() +
+                        "Found by name item" + System.lineSeparator() +
+                        "Menu." + System.lineSeparator() +
+                        "0.  Create " + System.lineSeparator() +
+                        "1.  All Items " + System.lineSeparator() +
+                        "2.  Edit " + System.lineSeparator() +
+                        "3.  Delete " + System.lineSeparator() +
+                        "4.  Find by Id " + System.lineSeparator() +
+                        "5.  Find by Name " + System.lineSeparator() +
+                        "6.  Exit " + System.lineSeparator() +
+                        "=== Exit program ===" + System.lineSeparator()
+                         , out.toString());
+    }
+
+    @Test
+    public void whenFindById() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("Found by ID item"));
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"4", item.getId(), "6"}
+        );
+        UserAction[] actions = {
+                new CreateAction(out),
+                new FindAllAction(out),
+                new EditAction(out),
+                new DeleteAction(out),
+                new FindByIdAction(out),
+                new FindByNameAction(out),
+                new ExitAction(out),
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertEquals("Menu." + System.lineSeparator() +
+                        "0.  Create " + System.lineSeparator() +
+                        "1.  All Items " + System.lineSeparator() +
+                        "2.  Edit " + System.lineSeparator() +
+                        "3.  Delete " + System.lineSeparator() +
+                        "4.  Find by Id " + System.lineSeparator() +
+                        "5.  Find by Name " + System.lineSeparator() +
+                        "6.  Exit " + System.lineSeparator() +
+                        "Found by ID item" + System.lineSeparator() +
+                        "Menu." + System.lineSeparator() +
+                        "0.  Create " + System.lineSeparator() +
+                        "1.  All Items " + System.lineSeparator() +
+                        "2.  Edit " + System.lineSeparator() +
+                        "3.  Delete " + System.lineSeparator() +
+                        "4.  Find by Id " + System.lineSeparator() +
+                        "5.  Find by Name " + System.lineSeparator() +
+                        "6.  Exit " + System.lineSeparator() +
+                        "=== Exit program ===" + System.lineSeparator()
+                        , out.toString());
     }
 }
