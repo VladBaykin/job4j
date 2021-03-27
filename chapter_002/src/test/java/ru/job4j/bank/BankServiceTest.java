1db956654cfc8b8504650e0d95c849c7e8987177
Vlad <vladbaykin@gmail.com>
2021-03-27 21:17:37 +0300

    Банковские переводы [#215091]

---
+++ chapter_002/src/test/java/ru/job4j/bank/BankServiceTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.bank;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BankServiceTest {
+
+    @Test
+    public void addUser() {
+        User user = new User("3434", "Petr Arsentev");
+        BankService bank = new BankService();
+        bank.addUser(user);
+        assertEquals(user, bank.findByPassport("3434"));
+    }
+
+    @Test
+    public void whenEnterInvalidPassport() {
+        User user = new User("3434", "Petr Arsentev");
+        BankService bank = new BankService();
+        bank.addUser(user);
+        bank.addAccount(user.getPassport(), new Account("5546", 150D));
+        assertNull(bank.findByRequisite("34", "5546"));
+    }
+
+    @Test
+    public void addAccount() {
+        User user = new User("3434", "Petr Arsentev");
+        BankService bank = new BankService();
+        bank.addUser(user);
+        bank.addAccount(user.getPassport(), new Account("5546", 150D));
+        assertEquals(150D, bank.findByRequisite("3434", "5546").getBalance(), 0.1);
+    }
+
+    @Test
+    public void transferMoney() {
+        User user = new User("3434", "Petr Arsentev");
+        BankService bank = new BankService();
+        bank.addUser(user);
+        bank.addAccount(user.getPassport(), new Account("5546", 150D));
+        bank.addAccount(user.getPassport(), new Account("113", 50D));
+        bank.transferMoney(user.getPassport(), "5546", user.getPassport(), "113", 150D);
+        assertEquals(200D, bank.findByRequisite(user.getPassport(), "113").getBalance(),0.1);
+    }
+}
