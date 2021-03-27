3339ae82e09b27304d2508135a2fc945de9576d4
Vlad <vladbaykin@gmail.com>
2021-03-27 18:11:54 +0300

    Паспорт и жители [#215094] добавил тест

--- chapter_002/src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ chapter_002/src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -13,4 +13,13 @@ public class PassportOfficeTest {
         office.add(citizen);
         assertEquals(citizen, office.get(citizen.getPassport()));
     }
+
+    @Test
+    public void whenAddFalse() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen2 = new Citizen("2f44a", "Name");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertFalse(office.add(citizen2));
+    }
 }
