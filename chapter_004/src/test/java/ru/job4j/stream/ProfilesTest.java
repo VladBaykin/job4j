eff4abb6996a48fc08af1ed3fe86ca5857a4d0a4
Vlad <vladbaykin@gmail.com>
2021-04-09 18:36:18 +0300

    Уникальность элементов и сортировка [#214958]

--- chapter_004/src/test/java/ru/job4j/stream/ProfilesTest.java
+++ chapter_004/src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -19,4 +19,18 @@ public class ProfilesTest {
         List<Address> expected = List.of(address, address2);
         assertEquals(expected, result);
     }
+
+    @Test
+    public void collectNoDuplicates() {
+        Address address = new Address("City", "Street", 1, 2);
+        Address address2 = new Address("City2", "Street2", 2, 3);
+        Address address3 = new Address("City2", "Street2", 2, 3);
+        Profile profile = new Profile(address);
+        Profile profile2 = new Profile(address2);
+        Profile profile3 = new Profile(address3);
+        List<Profile> profiles = List.of(profile, profile2, profile3);
+        List<Address> result = Profiles.collect(profiles);
+        List<Address> expected = List.of(address, address2);
+        assertEquals(expected, result);
+    }
 }
